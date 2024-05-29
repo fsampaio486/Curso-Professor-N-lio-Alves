be5a696d46cd2f8f6d5ebc4200bec132152279f3
Franciele Sampaio <121132995+fsampaio486@users.noreply.github.com>
2024-05-29 20:51:09 -0300

    Update and rename Exercicio_04.java to Lista de exercícios/Exercicio_04.java

--- Lista de exercícios/Exercicio_04.java
+++ Lista de exercícios/Exercicio_04.java
@@ -1,8 +1,8 @@
 package Modulo;
 
 /*
- * Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por 
- * hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por 
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
  */
 
 import java.util.Locale;
