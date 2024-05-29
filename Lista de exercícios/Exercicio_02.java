7bb1684dbbead65145ae8db9c91859f9b8c92019
Franciele Sampaio <121132995+fsampaio486@users.noreply.github.com>
2024-05-29 20:50:40 -0300

    Update and rename Exercicio_02.java to Lista de exercícios/Exercicio_02.java

--- Lista de exercícios/Exercicio_02.java
+++ Lista de exercícios/Exercicio_02.java
@@ -1,10 +1,10 @@
 package Modulo;
 
 /*
- * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
- * casas decimais.
- * Fórmula da área: area = π . raio2
- * Considere o valor de π = 3.14159
+ Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+ casas decimais.
+ Fórmula da área: area = π . raio2
+ Considere o valor de π = 3.14159
  */
 
 import java.util.Scanner;
